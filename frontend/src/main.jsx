144983cccbcadc929e182c92be5b4bf6669cf680
Gargi Srivastava <gargisrivastava@Gargis-MacBook-Pro.local>
2026-02-28 16:18:15 +0530

    rgy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,15 +5,12 @@ import App from "./App";
 import "./styles/globals.css";
 import { CartProvider } from "./context/CartContext";
 
-// import { Toaster } from "react-hot-toast";
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CartProvider>
       <BrowserRouter>
         <App />
-        {/* <Toaster position="top-right" /> */}
       </BrowserRouter>
     </CartProvider>
   </React.StrictMode>
